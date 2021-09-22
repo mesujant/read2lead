44b4308ba629c39174ecccbf4ac8e305970c0e49
synced123 <st420092@gmail.com>
2021-09-22 17:40:58 +0545

    Wed Sep 22 17:40:29 2021commit

---
+++ python/all_file_git_upload.py
@@ -0,0 +1,24 @@
+import os
+import time
+
+#path = "/home/sujan/Documents/read2lead"
+
+
+def git_upload_files(path):
+	os.chdir(path)
+	msg = time.ctime(time.time()) + "commit"
+	git_add = "git add *"
+	git_commit = "git commit -m {}".format(msg)
+	git_push = "git push origin main"
+	
+	os.system(git_add)
+	os.system(git_commit)
+	os.system(git_push)
+	#print(os.listdir(os.getcwd()))
+
+
+if __name__ == '__main__':
+	path = "/home/sujan/Documents/read2lead"
+	git_upload_files(path)
+
+
