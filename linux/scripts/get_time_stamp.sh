f12175a2be851be483f1fe914842f9c762eac6ec
synced123 <st420092@gmail.com>
2021-10-25 17:43:39 +0545

    Mon Oct 25 17:43:39 2021commit

--- linux/scripts/get_time_stamp.sh
+++ linux/scripts/get_time_stamp.sh
@@ -4,6 +4,9 @@
 #now the question is to traverse through the directories and find the latest modified file 
 #echo $(ls -alt | head -2 | tail -1) | awk '{print $6, $7, $8}'
 
+
+
+
 for i in `ls` 
     do
       echo `ls -l $i` > test_output.txt;
