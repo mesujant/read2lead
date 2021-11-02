0222ed039e3ed503c7a41b0963f71b1bab528a7c
synced123 <st420092@gmail.com>
2021-11-02 16:49:39 +0545

    Tue Nov  2 16:49:38 2021commit

--- python/check_availability.py
+++ python/check_availability.py
@@ -3,12 +3,24 @@
 
 import requests
 
-conn_timeout = 2
-read_timeout = 60
-timeouts = (conn_timeout, read_timeout)
-r = requests.get('https://onlineedlreg.dotm.gov.np/', timeout=timeouts)
+def check_availability(url):
+	conn_timeout = 2
+	read_timeout = 60
+	timeouts = (conn_timeout, read_timeout)
+	r = requests.get(url, timeout=timeouts)
+	return r.ok
 
-if r.ok:
-	print("found")
-else:
-	print("not found")
+def send_notification():
+	pass
+
+if __name__ == '__main__':
+	url = 'https://onlineedlreg.dotm.gov.np/'
+	#print(check_availability(url))
+	 
+	while not check_availability(url):
+		print("page not found:")
+
+	send_notification()
+
+
+ 
