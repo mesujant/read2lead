8a1ce735a2e42457454f0d24c6a0605cc670f34f
synced123 <st420092@gmail.com>
2021-12-04 21:01:55 +0545

    Sat Dec  4 21:01:54 2021commit

--- python/horizon/horizon_reporting.py
+++ python/horizon/horizon_reporting.py
@@ -1,13 +1,12 @@
 import os
 
-user_file_path = "/home/sujan/Documents/read2lead/horizon/horizon_users/"
- 
+user_file_path = os.getcwd() + "/"
 
 def read_file(file):
 	users = []
 	with open(file, 'r') as f:
 		users = f.readlines()
-		#print(f.readlines())
+		##print(f.readlines())
 	return [user.strip() for user in users if len(user) > 2]
 
 def get_total_user_count(all_users):
@@ -15,7 +14,7 @@ def get_total_user_count(all_users):
 	for pool, users in all_users:
 		unique_users += users
 
-	with open("/home/sujan/Desktop/report.txt", 'a') as f:
+	with open("total_users.txt", 'a') as f:
 		for user in set(unique_users):
 			f.write(user+"\n")
 
@@ -41,35 +40,35 @@ def display_result(user_pools):
 	f2 = open('users_in_multiple_pool.txt', 'a')
 	f = open('user_associated_pool.txt','a')
 
-	print("\n##########################################\n")
+	#print("\n##########################################\n")
 	for user, pool in user_pools:
 		if len(pool) == 1:
-			print(user , pool)
+			#print(user , pool)
 			f.write("\n" + user + " " + str(pool))
 			one_pool_user += 1
 	f.write("\n\n")
 
-	print("\n##########################################\n")
-	print("Total one Pool User :", one_pool_user)
-	print("\n##########################################\n")
-	print("\n##########################################\n")
+	#print("\n##########################################\n")
+	#print("Total one Pool User :", one_pool_user)
+	#print("\n##########################################\n")
+	#print("\n##########################################\n")
 	for user, pool in user_pools:
 		if len(pool) == 2:
-			print(user, pool)
+			#print(user, pool)
 			f.write("\n" + user + " " + str(pool))
 			f2.write("\n" + user + " " + str(pool))
 			two_pool_user += 1
 
 	f.write("\n\n")
 
-	print("\n##########################################\n")
-	print("Total two Pool User :", two_pool_user)
-	print("\n##########################################\n")
-	print("\n##########################################\n")
+	#print("\n##########################################\n")
+	#print("Total two Pool User :", two_pool_user)
+	#print("\n##########################################\n")
+	#print("\n##########################################\n")
 
 	for user, pool in user_pools:
 		if len(pool) == 3:
-			print(user, pool)
+			#print(user, pool)
 			f.write("\n" + user + " " + str(pool))
 			f2.write("\n" + user + " " + str(pool))
 			three_pool_user += 1
@@ -77,42 +76,67 @@ def display_result(user_pools):
 	f.write("\n\n")
 
 
-	print("\n##########################################\n")
-	print("Total three Pool User :", three_pool_user)
-	print("\n##########################################\n")
-	print("\n##########################################\n")
+	#print("\n##########################################\n")
+	#print("Total three Pool User :", three_pool_user)
+	#print("\n##########################################\n")
+	#print("\n##########################################\n")
 
 	for user, pool in user_pools:
 		if len(pool) == 4:
-			print(user, pool)
+			#print(user, pool)
 			f.write("\n" + user + " " + str(pool))
 			f2.write("\n" + user + " " + str(pool))
 			four_pool_user += 1
 	f.write("\n\n")
 
 
-	print("\n##########################################\n")
-	print("Total four Pool User :", four_pool_user)
-	print("\n##########################################\n")
-	print("\n##########################################\n")
+	#print("\n##########################################\n")
+	#print("Total four Pool User :", four_pool_user)
+	#print("\n##########################################\n")
+	#print("\n##########################################\n")
 
 
 
-def inactive_users_associated_pools(inactive_users_file, user_pools_file):
+def inactive_users_associated_pools():
 	#find pools associated with inactive for more than 30 days:
 	#read inactive users list
 	#find pools from the user_pools list
+
 	inactive_users = []
 	user_pools = []
 	inactive_user_pools = []
-	with open(inactive_user_pools, 'r') as f:
-		inactive_users = r.readlines()
+	with open('inactive_user_for_30_days.txt', 'r') as f:
+		inactive_users = [user.strip() for user in f.readlines() if len(user) > 2]
 
-	with open(user_pools_file, 'r') as f:
-		user_pools = r.readlines()
+	with open('user_associated_pool.txt', 'r') as f:
+		user_pools = [user_pool.strip() for user_pool in f.readlines() if len(user_pool) > 2 ]
 
+	
 	for inactive_user in inactive_users:
-		print(inactive_users)
+		#print(user_pool.split(" "))
+
+		 
+		#print(inactive_user)
+
+		for user_pool in user_pools:
+			if inactive_user in user_pool:
+				inactive_user_pools.append(user_pool)
+				#print(user_pool)
+				#input
+				
+				break
+
+
+	with open('inactive_users_associated_pools.txt', 'a') as f:
+		for user_pool in sorted(inactive_user_pools, key = lambda i: len(i), reverse=True):
+			f.write(user_pool + "\n", )
+			
+
+
+
+	#print(inactive_users)
+	#input("\n")
+	#print(user_pools)
 
 
 
@@ -124,18 +148,19 @@ def notify_if_multiple_pool_assigned():
 def get_report(all_users):
 	#total horizon users
 	#users with multiple pool asssigned
-	#users active 
+	#users active and associated vm
 	#users inactive
 
 
 	total_horizon_users, all_horizon_users = get_total_user_count(all_users)
-	print(total_horizon_users, all_horizon_users)
+	#print(total_horizon_users, all_horizon_users)
 	
 	user_pools = get_user_associated_pool(all_horizon_users, all_users)
 
 	#write user associated pool in file:
 
 	display_result(user_pools)
+	inactive_users_associated_pools()
 
 
 # def read_file(user_file_path):
@@ -149,9 +174,9 @@ def get_report(all_users):
 if __name__ == "__main__":
 	#read_file(user_file_path)
 
-	inactive_user_file = "inactive_user_for_30_days.txt"
-	user_pool_file = "user_associated_pool.txt"
-	inactive_users_associated_pools(inactive_users_file, )
+	# inactive_user_file = "inactive_user_for_30_days.txt"
+	# user_pool_file = "user_associated_pool.txt"
+	# inactive_users_associated_pools(inactive_users_file, )
 
 	all_users = []
 	for file in os.listdir(user_file_path):
